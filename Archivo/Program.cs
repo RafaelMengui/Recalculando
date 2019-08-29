9e70833b453253fea7ab55360205dd54ae6940ee
Agusmarrero <53953901+Agusmarrero@users.noreply.github.com>
2019-08-29 10:41:24 -0300

    Crear archivo

---
+++ Archivo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Archivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
