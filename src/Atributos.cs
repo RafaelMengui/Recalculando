5bc0795217ebc1778be34ab2989430ced718c753
RafaxMg <48605323+RafaxMg@users.noreply.github.com>
2019-09-12 20:06:47 -0300

    Añado la clase Atributos y program

---
+++ src/Atributos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace src
+{
+    public class Atributos
+    {
+        public string Clave { get; set; }
+        public string Valor { get; set; }
+
+        public Atributos(string clave, string valor)
+        {
+            this.Clave = clave;
+            this.Valor = valor;
+        }
+    }
+}
