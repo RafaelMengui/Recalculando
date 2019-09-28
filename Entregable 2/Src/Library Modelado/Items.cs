f9c6d33d46122cff367be0b17728e8c61cf8aa27
NicoPuig <nicopuig14@hotmail.com>
2019-09-28 18:52:26 -0300

    Abstract Class Item y arreglos menores

---
+++ Entregable 2/Src/Library Modelado/Items.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Modelado
+{
+    /// <summary>
+    /// Clase abstracta de Items.
+    /// </summary>
+    public abstract class Item
+    {
+        private string Name;
+        private string Position_X;
+        private string Position_Y;
+        private string Size;
+        private string Level;
+        private bool Draggable;
+
+        public string name { get; set; }
+        public string position_x { get; set; }
+        public string position_y { get; set; }
+        public string size { get; set; }
+        public string level { get; set; }
+        public bool draggable { get; set; }
+
+        public Item(string name, string level, string position_x, string position_y, string size, bool draggable)
+        {
+            this.Name = name;
+            this.Level = level;
+            this.Position_X = position_x;
+            this.Position_Y = position_y;
+            this.Size = size;
+            this.Draggable = draggable;
+        }
+    }
+}
--- Entregable 2/Src/Program/Program.cs
+++ Entregable 2/Src/Program/Program.cs
@@ -6,6 +6,7 @@
 
 using System;
 using LeerHTML;
+using Modelado;
 
 namespace App
 {
