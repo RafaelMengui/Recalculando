c04bdf2173425385f25b76192b7e5ad84915d39c
NicoPuig <nicopuig14@hotmail.com>
2019-10-01 21:13:20 -0300

    Ordenar Carpetas y Builder

---
+++ Entregable 2/Src/StudentsCode/Builder.cs
@@ -0,0 +1,16 @@
+using System;
+using Proyecto.Common;
+
+namespace Modelado
+{
+    /// <summary>
+    /// Clase abstracta de Items.
+    /// </summary>
+    public class Builder : IBuilder
+    {
+        public void Build(IMainViewAdapter adapter)
+        {
+          
+        }
+    }
+}
