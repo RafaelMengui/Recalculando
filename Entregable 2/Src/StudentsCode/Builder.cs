e4121339e604a179cafac34ec11f8901cafcbf88
NicoPuig <nicopuig14@hotmail.com>
2019-10-02 00:07:52 -0300

    Arreglo Estructuras

--- Entregable 2/Src/StudentsCode/Builder.cs
+++ Entregable 2/Src/StudentsCode/Builder.cs
@@ -1,16 +1,28 @@
+//--------------------------------------------------------------------------------
+// <copyright file="Builder.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
 using System;
 using Proyecto.Common;
 
-namespace Modelado
+namespace Proyecto.StudentsCode
 {
     /// <summary>
-    /// Clase abstracta de Items.
+    /// Clase que implementa la interfaz IBuilder.
+    /// Tiene la responsabilidad de generar los archivos 'StudentsCode.dll' y 'Common.dll'.
     /// </summary>
     public class Builder : IBuilder
     {
+        /// <summary>
+        /// Construye una interfaz de usuario interactiva utilizando.
+        /// un <see cref="IMainViewAdapter"/>.
+        /// </summary>
+        /// <param name="adapter">Un <see cref="IMainViewAdapter"/> que permite construir
+        /// una interfaz de usuario interactiva.</param>
         public void Build(IMainViewAdapter adapter)
         {
-          
         }
     }
 }
--- Entregable 2/Src/StudentsCode/Items.cs
+++ Entregable 2/Src/StudentsCode/Items.cs
@@ -1,34 +1,114 @@
-﻿using System;
+﻿//--------------------------------------------------------------------------------
+// <copyright file="Items.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
 
-namespace Modelado
+namespace Proyecto.StudentsCode
 {
     /// <summary>
     /// Clase abstracta de Items.
     /// </summary>
-    public abstract class Item
+    public abstract class Items
     {
-        private string Name;
-        private string Position_X;
-        private string Position_Y;
-        private string Size;
-        private string Level;
-        private bool Draggable;
+        /// <summary>
+        /// Nombre del Item.
+        /// </summary>
+        private string name;
 
-        public string name { get; set; }
-        public string position_x { get; set; }
-        public string position_y { get; set; }
-        public string size { get; set; }
-        public string level { get; set; }
-        public bool draggable { get; set; }
+        /// <summary>
+        /// Nivel al que pertenece el Item.
+        /// </summary>
+        private string level;
 
-        public Item(string name, string level, string position_x, string position_y, string size, bool draggable)
+        /// <summary>
+        /// Posicion en eje Horizontal en pixeles.
+        /// </summary>
+        private string positionX;
+
+        /// <summary>
+        /// Posicion en eje Vertical en pixeles.
+        /// </summary>
+        private string positionY;
+
+        /// <summary>
+        /// Ancho en Pixeles.
+        /// </summary>
+        private string width;
+
+        /// <summary>
+        /// Altura en pixeles.
+        /// </summary>
+        private string height;
+
+        /// <summary>
+        /// Bool que define si un item es arrastable o no.
+        /// </summary>
+        private bool draggable;
+
+        /// <summary>
+        /// Inicializa una instancia de Item.
+        /// </summary>
+        /// <param name="Name">Nombre del Item.</param>
+        /// <param name="Level">Nivel al que pertence.</param>
+        /// <param name="PositionX">Posicion en eje horizontal en pixeles.</param>
+        /// <param name="PositionY">Posicion en eje vertical en pixeles.</param>
+        /// <param name="Width">Ancho en pixeles.</param>
+        /// <param name="Height">Altura en pixeles.</param>
+        /// <param name="Draggable">Item arrastrable.</param>
+        public Items(string Name, string Level, string PositionX, string PositionY, string Width, string Height, bool Draggable)
         {
-            this.Name = name;
-            this.Level = level;
-            this.Position_X = position_x;
-            this.Position_Y = position_y;
-            this.Size = size;
-            this.Draggable = draggable;
+            this.name = Name;
+            this.level = Level;
+            this.positionX = PositionX;
+            this.positionY = PositionY;
+            this.width = Width;
+            this.height = Height;
+            this.draggable = Draggable;
         }
+
+        /// <summary>
+        /// Gets or sets del nombre del item.
+        /// </summary>
+        /// <value>String nombre del item.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets del Nivel al que pertenece el Item.
+        /// </summary>
+        /// <value>String nivel al que pertence.</value>
+        public string Level { get; set; }
+
+        /// <summary>
+        /// Gets or sets de Posicion en eje Horizontal en pixeles.
+        /// </summary>
+        /// <value>String posicion en eje horizontal.</value>
+        public string PositionX { get; set; }
+
+        /// <summary>
+        /// Gets or sets dePosicion en eje Vertical en pixeles.
+        /// </summary>
+        /// <value>String posicion en eje vertical.</value>
+        public string PositionY { get; set; }
+
+        /// <summary>
+        /// Gets or sets de Ancho en Pixeles.
+        /// </summary>
+        /// <value>String ancho en pixeles.</value>
+        public string Width { get; set; }
+
+        /// <summary>
+        /// Gets or sets de Altura en pixeles.
+        /// </summary>
+        /// <value>String altura en pixeles.</value>
+        public string Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether un item es arrastable o no.
+        /// Escribo algunas partes en ingles porque el docfx me obliga a que aparezca esa
+        /// frase.
+        /// </summary>
+        /// <value>Bool de si es arrastrable.</value>
+        public bool Draggable { get; set; }
     }
 }
