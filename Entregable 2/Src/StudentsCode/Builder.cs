1dfc55fa67fc51dd3125f5cc8ed8b7b0e588d53d
RafaxMg <48605323+RafaxMg@users.noreply.github.com>
2019-10-06 19:27:34 -0300

    Eso lo sabremos pronto...

--- Entregable 2/Src/StudentsCode/Builder.cs
+++ Entregable 2/Src/StudentsCode/Builder.cs
@@ -19,32 +19,96 @@ namespace Proyecto.StudentsCode
     /// 
     public class Builder : IBuilder
     {
-        /// <summary>
+        private string firstPageName;
+        private IMainViewAdapter adapter;
+        private string nextPageName;      
+
+        static World world;  
+//////////////////////////////////////////////////////////////////////////////
+        /// /// <summary>
         /// Construye una interfaz de usuario interactiva utilizando.
         /// un <see cref="IMainViewAdapter"/>.
         /// </summary>
-        /// <param name="adapter">Un <see cref="IMainViewAdapter"/> que permite construir
+        /// <param name="providedAdapter">Un <see cref="IMainViewAdapter"/> que permite construir
         /// una interfaz de usuario interactiva.</param>
-        public void Build(IMainViewAdapter adapter)
+        public void Build(IMainViewAdapter providedAdapter)
         {
+
+            this.adapter = providedAdapter ?? throw new ArgumentNullException(nameof(providedAdapter));
+
+            this.adapter.ToDoAfterBuild(this.AfterBuildShowFirstPage);
+
+            this.firstPageName = this.adapter.AddPage();
+
+            this.adapter.ChangeLayout(Layout.ContentSizeFitter);         
+
+            
+
+
             string XMLfile = @"..\..\..\Archivos HTML\Ejemplo.xml";
             List<Tag> tags = Filtro.FiltrarHTML(LeerHtml.RetornarHTML(XMLfile));
 
-            foreach (Tag tag in tags)
+            foreach (Tag tag in tags) //Se crean los objetos C#
             {
                 switch (tag.Nombre)
                 {
                     case "World":
-                        var world = Creator.AddWorld(tag);
-                    break;
+                        world = Creator.AddWorld(tag);
+                        break;
                     case "Level":
-                        Console.WriteLine("The color is green");
+                        Space level = Creator.AddLevel(tag);
                         break;
-                    case "Items":
-                        Console.WriteLine("The color is blue");
+                    case "Button":
+                        Items button = Creator.AddButton(tag);
                         break;
+                    case "Image":
+                        Items image = Creator.AddImage(tag);
+                        break;
+                    case "DragAndDropSource":
+                        Items dragAndDropSource = Creator.AddDragAndDropSource(tag);
+                        break;
+                    case "DragAndDropDestination":
+                        Items DragAndDropDestination = Creator.AddDragAndDropDestination(tag);
+                        break;
+                    case "DragAndDropItem":
+                        Items DragAndDropItem = Creator.AddDragAndDropItem(tag);
+                        break;
+
                 }
             }
+
+            //Crear los objetos Unity            
+            foreach (Space level in world.SpaceList)
+            {
+                foreach (Items unityItem in level.ItemList)
+                {
+                    unityItem.CreateUnityItem(adapter);
+                }
+            }
+
+
         }
+
+        /// <summary>
+        /// After Build Show First Page
+        /// </summary>
+        public void AfterBuildShowFirstPage()
+        {
+            this.adapter.ShowPage(this.firstPageName);
+        }
+
+        private void GoToFirstPage()
+        {
+            this.adapter.ShowPage(this.firstPageName);
+            this.adapter.PlayAudio("Speech On.wav");
+        }
+
+        private void GoToNextPage()
+        {
+            this.adapter.ShowPage(this.nextPageName);
+            this.adapter.PlayAudio("Speech Off.wav");
+        }
+
+
     }
 }
