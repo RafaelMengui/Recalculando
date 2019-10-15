e6f8442758ad1d5f919e707e2b5741ee2a7d1d7a
RafaelMengui <rafael.mengui@correo.ucu.edu.uy>
2019-10-14 22:18:01 -0300

    Modelado de Alimentos y Bowl para el nivel de cocina.

--- Entregable 2/Src/LibraryModelado/Item/NivelCocina/Bowl.cs
+++ Entregable 2/Src/LibraryModelado/Item/NivelCocina/Bowl.cs
@@ -1,21 +1,16 @@
-//--------------------------------------------------------------------------------
-// <copyright file="DragAndDropDestination.cs" company="Universidad Católica del Uruguay">
-//     Copyright (c) Programación II. Derechos reservados.
-// </copyright>
-//--------------------------------------------------------------------------------
-using System.Collections.Generic;
+using System;
 using Proyecto.Common;
 using Proyecto.LibraryModelado;
 
-namespace Proyecto.Item
+namespace Proyecto.Item.NivelCocina
 {
-    /// <summary>
-    /// Clase DragAndDropDestination. Hereda de <see cref="Items"/>.
+       /// <summary>
+    /// Clase Bowl. Hereda de <see cref="Items"/>.
     /// </summary>
-    public class DragAndDropDestination : Items
+    public class Bowl : Items
     {
         /// <summary>
-        /// Constructor. Instancia Objetos DragAndDropDestination.
+        /// Constructor. Instancia Objetos Bowl .
         /// </summary>
         /// <param name="name">Nombre del Item.</param>
         /// <param name="level">Nivel al que pertence.</param>
@@ -24,13 +19,15 @@ namespace Proyecto.Item
         /// <param name="width">Ancho en pixeles.</param>
         /// <param name="height">Altura en pixeles.</param>
         /// <param name="image">Imagen del Item.</param>
-        public DragAndDropDestination(string name, Space level, int positionX, int positionY, int width, int height, string image)
+        public Bowl(string name, Space level, int positionX, int positionY, int width, int height, string image)
         : base(name, level, positionX, positionY, width, height, image)
         {
-            this.SavedItems = new List<DragAndDropItem>();
+            this.InTheBowl = new List<Food>();
+            this.IncorrectItems = new List<DragAndDropItem>();
         }
 
-        public List<DragAndDropItem> SavedItems {get; set;}     
+        public List<Food> InTheBowl {get; set;}   
+        public List<DragAndDropItem> IncorrectItems {get; set;}       
 
         /// <summary>
         /// Metodo para crear DragAndDropDestination en Unity.
