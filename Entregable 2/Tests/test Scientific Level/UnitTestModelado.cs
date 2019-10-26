1a48e138a1e08bedc1f0da50afa7ed8a4d029706
Agusmarrero <53953901+Agusmarrero@users.noreply.github.com>
2019-10-26 14:12:27 -0300

    Test

--- Entregable 2/Tests/test Scientific Level/UnitTestModelado.cs
+++ Entregable 2/Tests/test Scientific Level/UnitTestModelado.cs
@@ -22,10 +22,7 @@ namespace Proyecto.LibraryModelado.Engine.test
         [Fact]
         public void TestVerifyAddition()
         {
-            /// <summary>
-            /// Este Test se encaarga de verificar que las sumas estén hechas de forma correcta.
-            /// </summary>
-            /// <returns></returns>
+        
             MoneyContainer dragcontainerSource = new MoneyContainer("source", level, 1, 10, 10, 10, null, 0);
             MoneyContainer dragcontainerDestintantion = new MoneyContainer("dragC", level, 1, 10, 10, 10, null, 120);
             Money moneydrag = new Money("drag", level, 20, 20, 2, 2, null, true, dragcontainerSource, 120);
@@ -35,10 +32,6 @@ namespace Proyecto.LibraryModelado.Engine.test
         [Fact]
         public void TestOnDrop()
         {
-            /// <summary>
-            /// 
-            /// </summary>
-            /// <returns></returns>
 
             MoneyContainer dragSource = new MoneyContainer("drag", level, 1, 10, 10, 10, null, 1);
             MoneyContainer dragcontainerDestintantion = new MoneyContainer("dragC", level, 1, 10, 10, 10, null, 120);
@@ -58,11 +51,6 @@ namespace Proyecto.LibraryModelado.Engine.test
         [Fact]
         public void NegativeMoneyValue()
         {
-            /// <summary>
-            /// Nuestro juego no permite que los billetes tengan valores negativos, por esto,
-            /// este test en caso de que esto suceda, ejecuta la excepción y convierte al valor en positivo.
-            /// </summary>
-            /// <returns></returns>
             MoneyContainer container1 = new MoneyContainer("cont1", this.level, 50, 50, 60, 60, null, 10);
             Assert.Throws<ArithmeticException>(() => new Money("name", this.level, 50, 40, 30, 20, null, false, container1, -10));
         }
@@ -70,11 +58,6 @@ namespace Proyecto.LibraryModelado.Engine.test
         [Fact]
         public void WinPage()
         {
-            /// <summary>
-            /// En este juego hay dos páginas, por esto, testeamos cuando se gana una página para luego
-            /// pasar a la próxima y así ganar el nivel.
-            /// </summary>
-            /// <returns></returns>
             MoneyContainer containerDestination1 = new MoneyContainer("Dest1", this.level, 50, 50, 60, 60, null, 10); // 60 - 50
             MoneyContainer containerDestination2 = new MoneyContainer("Dest2", this.level, 50, 50, 60, 60, null, 175); // 100 + 175
             MoneyContainer containerSource1 = new MoneyContainer("Source1", this.level, 50, 50, 60, 60, null, 0);
@@ -92,10 +75,6 @@ namespace Proyecto.LibraryModelado.Engine.test
         [Fact]
         public void WinLevel()
         {
-            /// <summary>
-            /// Verifica que haya ganado el nivel completo de juego.
-            /// </summary>
-            /// <returns></returns>
             MoneyContainer containerDestination1 = new MoneyContainer("Dest1", this.level, 50, 50, 60, 60, null, 10);
             MoneyContainer containerDestination2 = new MoneyContainer("Dest2", this.level, 50, 50, 60, 60, null, 175);
             MoneyContainer containerSource1 = new MoneyContainer("Source1", this.level, 50, 50, 60, 60, null, 0);
@@ -109,7 +88,6 @@ namespace Proyecto.LibraryModelado.Engine.test
 
             if (engineScientific.VerifyWinPage())
             {
-                // Pagina 2.
                 engineScientific.StartPage();
                 MoneyContainer containerDestination3 = new MoneyContainer("Dest3", this.level, 50, 50, 60, 60, null, 50);
                 MoneyContainer containerDestination4 = new MoneyContainer("Dest4", this.level, 50, 50, 60, 60, null, 200);
