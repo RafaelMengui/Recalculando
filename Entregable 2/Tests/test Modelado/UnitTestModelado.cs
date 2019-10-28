07c1e3534f30b668ce676fcbb3800da754462b09
RafaxMg <48605323+RafaxMg@users.noreply.github.com>
2019-10-27 23:45:58 -0300

    Cambios a master

--- Entregable 2/Tests/test Modelado/UnitTestModelado.cs
+++ Entregable 2/Tests/test Modelado/UnitTestModelado.cs
@@ -1,13 +1,30 @@
 using System;
+using System.Collections.Generic;
+using Proyecto.LeerHTML;
+using Proyecto.LibraryModelado.Engine;
+using Proyecto.Factory.CSharp;
+using Proyecto.Item;
+using Proyecto.Item.ScientistLevel;
 using Xunit;
 
-namespace Modelado.test
+namespace Proyecto.LibraryModelado.test
 {
     public class UnitTestModelado
     {
+
         [Fact]
-        public void Test1()
+        public void TestFactoryC()
         {
+            const string XMLfile = @"..\..\..\..\..\Src\ArchivosHTML\TesteoModelado.xml";
+
+            List<Tag> tags = Parser.ParserHTML(ReadHTML.ReturnHTML(XMLfile));
+            List<IComponent> componentList = new List<IComponent>();
+
+            foreach (Tag tag in tags)
+            {
+               Assert.Throws<System.ArgumentNullException>(() => FactoryComponent.InitializeFactories().MakeComponent(tag));
+            }
+
         }
     }
 }
